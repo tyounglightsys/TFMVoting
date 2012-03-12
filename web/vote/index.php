4594f920e7a2d3553e96bc07fb5dc69baff866d1
Net_C <mark@techfocus.net>
2012-03-12 16:51:39 -0600

    Further fleshing the user voting interface.

--- web/vote/index.php
+++ web/vote/index.php
@@ -5,6 +5,8 @@
  */
 
 require_once('../config.php');
+require_once('../components.php');
+require_once('../functions.php');
 
 ?>
 
@@ -16,25 +18,47 @@ require_once('../config.php');
     <body>
         <div id="header"><h1>Voting</h1></div>
         <div id="votingForm">
-            <form action="">
-                <div id="projectName"><?php echo $NAME ?></div>
-                <div id="sliderBox">
-                    <div>
-                    <span>Input</span> // mouseover for description <br />
-                    <input type="range" name="Impact" value="0" min="-10" max="10" />
-                    <?php
-                        /*
-                        foreach($DEFAULT_CRITERIA as $criteria) {
-                            print('<span on>');
-                            print('' . $criteria[0]);
-                            
-                            print('</span>');
-                        }
-                        */
-                    ?>
+            <form name="postVote" action="" method="post">
+                <table>
+                    <tr id="projectName">
+                        <td><h2>Project Name 1<?php echo $NAME ?></h2></td>
+                    </tr>
+                    <div id="sliderBox">
+                        <tr>
+                            <td style="width: 50px;" alt="testing!">Impact</td>
+                            <td><input type="range" name="Impact" value="0" min="-10" max="10" /></td>
+                        </tr>
+                        <!-- <?php
+                            /*
+                            foreach($DEFAULT_CRITERIA as $criteria) {
+                                print('<span on>');
+                                print('' . $criteria[0]);
+                                
+                                print('</span>');
+                            }
+                            */
+                        ?> -->
+                        <tr>
+                            <td>Quality</td>
+                            <td><input type="range" name="Quality" value="0" min="-10" max="10" /></td>
+                        </tr>
+                        <tr>
+                            <td>Co-operation</td>
+                            <td><input type="range" name="Cooperation" value="0" min="-10" max="10" /></td>
+                        </tr>
+                        <tr>
+                            <td>Re-usability</td>
+                            <td><input type="range" name="Reusability" value="0" min="-10" max="10" /></td>
+                        </tr>
                     </div>
-                </div>
+                    <tr>
+                        <td><input type="submit" name="submit" /></td>
+                    </tr>
+                </table>
             </form>
+        </div>
+        <div id="showVoted">
+            <div id="projectName"><?php echo $NAME ?></div>
         </div>
     </body>
 </html>
