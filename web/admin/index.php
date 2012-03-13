b7db1a672b3a2e761793d2c10ac983a0d0229bb8
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2012-03-12 23:20:19 -0600

    Implementing more of the admin UI.
    Now we have the states reflected in the UI and there were some
    corrections made to the functions.  Also, you are able to set what
    project set you are viewing.

--- web/admin/index.php
+++ web/admin/index.php
@@ -2,59 +2,114 @@
 
 /** \file index.php
  * \brief This file is the administrative view.
+ *
+ * This file takes the following GET parameters: projectSet - This is the
+ * current group of entries to work on, which will default to the most recent
+ * if none is given.
  */
 
+// Requires --------------------------------------------------------------------
 require_once("../config.php");
 require_once("../components.php");
 require_once("../functions.php");
 
-DB_Start(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+// Variables -------------------------------------------------------------------
+$projectSet = null;
+$title = "";
+$states = array("resultsVisible" => false, "votingOpen" => false, "archived" => false);
+
+// Generation ------------------------------------------------------------------
+DB_Start();
+
+// Get current project set
+if(isset($_GET["projectSet"]) && DB_GetProjectSetExists($_GET["projectSet"])){
+    $projectSet = $_GET["projectSet"];
+}
+else{
+    $projectSet = DB_GetCurrentProjectSetName();
+    if(!$projectSet){
+        $projectSet = null;
+    }
+}
+
+$title = htmlentities($projectSet) . " > Administration";
+$states = DB_GetProjectSetStates($projectSet);
 
 ?>
 
 <html>
     <head>
-        <title> TFM Voting Administration </title>
+        <title> <?php print($title) ?> </title>
         <link href="../style.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
         
         <div id="header">
+            <h1> <?php print($title) ?></h1>
             <div id="projectSetActions"> 
                 <form action="index.php">
                     <select name="projectSet">
+                        
                         <?php
                             // Generate list of projects
                             $names = DB_GetAllProjectSetNames();
                             foreach($names as $name){
-                                printf('<option value="%s" selected="true"> ' . htmlentities($name) .  ' </option>', $name);
+                                printf('<option value="%s" %s>%s</option>',
+                                       htmlspecialchars($name, ENT_QUOTES),
+                                       ($name == $projectSet) ? "selected='true'" : "",
+                                       htmlentities($name));
                             }
                         ?>
+    
                     </select>
                     <input type="submit" value="Change Project Set" />
                     <br />
                     <input type="submit" value="New Project Set" />
                 </form>
             </div>
-            <h1>Administration</h1>
         </div>
         
-        <h2> Project Set Status </h2>
+        <h2> Status </h2>
         <form action="">
             <table>
                 <tr>
                     <td>Open:</td>
-                    <td><input type="checkbox" name="votingOpen" /></td>
+                    <td><input type="checkbox" name="votingOpen"
+                        
+                        <?php
+                            // Show the status for the votingOpen category
+                            if($states["votingOpen"]){
+                                print("checked='yes'");
+                            }
+                        ?>"/>
+                        
+                    </td>
                 </tr>
-                
                 <tr>
                     <td>Showing Results:</td>
-                    <td><input type="checkbox" name="showingResults" /></td>
+                    <td><input type="checkbox" name="showingResults" 
+                    
+                        <?php
+                            // Show the status for the resultsVisible category
+                            if($states["resultsVisible"]){
+                                print("checked='yes'");
+                            }
+                        ?>"/>
+                        
+                    </td>
                 </tr>
-                
                 <tr>
                     <td>Archived: </td>
-                    <td><input type="checkbox" name="archived" /></td>
+                    <td><input type="checkbox" name="archived"
+                    
+                        <?php
+                            // Show the status for the archived category
+                            if($states["archived"]){
+                                print("checked='yes'");
+                            }
+                        ?>"/>
+                    
+                    </td>
                 </tr>
             </table>
         </form>
--- web/functions.php
+++ web/functions.php
@@ -6,7 +6,20 @@
  * \{
  */
 
-function DB_Start($host, $user, $password, $database){
+// Requires --------------------------------------------------------------------
+require_once("config.php");
+
+// Functions -------------------------------------------------------------------
+
+/** \brief Connect to the database for the data.
+ *
+ * This never returns a failure, as it dies on failure.
+ * \param host The host to connect to.
+ * \param user The username to log in with.
+ * \param password The password to log in to the database with.
+ * \param database The database in which all of the table data resides.
+ */
+function DB_Start($host = DB_HOST, $user = DB_USERNAME, $password = DB_PASSWORD, $database = DB_DATABASE){
     mysql_connect($host, $user, $password) or die(mysql_error());
     mysql_selectdb($database) or die(mysql_error());
 }
@@ -19,8 +32,20 @@ function DB_GetProjectSetCriteria($projectSetName){
     
 }
 
-function DB_GetProjectStates($projectSetName){
-    
+/** \brief This returns an associative array of the different states of the
+ * project set that is requested.
+ *
+ * The returned associative array will have the keys "resultsVisible",
+ * "votingOpen", and "archived".
+ * \param projectSetName The name of the project set to query the states of.
+ * \return This returns an associative array of the states that are associated
+ * with the project set.  This returns false on error, such as not finding the
+ * specified project set.
+ */
+function DB_GetProjectSetStates($projectSetName){
+    $res = mysql_query("SELECT `resultsVisible`, `votingOpen`, `archived`  FROM `set` WHERE `name` = '" . mysql_real_escape_string($projectSetName) . "'") or die(mysql_error());
+    $row = mysql_fetch_array($res);
+    return $row;
 }
 
 /** \brief Get the most recently made project set name from the database.
@@ -28,13 +53,13 @@ function DB_GetProjectStates($projectSetName){
  * current project.
  */
 function DB_GetCurrentProjectSetName(){
-    $res = mysql_query("SELECT `name`  FROM `set` ORDER BY `date` LIMIT 1") or die(mysql_error());
+    $res = mysql_query("SELECT `name`  FROM `set` ORDER BY `date` DESC LIMIT 1") or die(mysql_error());
     $row = mysql_fetch_row($res);
     if($row){
         return $row[0];
     }
     else{
-        return null;
+        return false;
     }
 }
 
@@ -42,7 +67,7 @@ function DB_GetCurrentProjectSetName(){
  * \return This returns an array of names of the projects.
  */
 function DB_GetAllProjectSetNames(){
-    $res = mysql_query("SELECT `name` FROM `set` ORDER BY `date`") or die(mysql_error());
+    $res = mysql_query("SELECT `name` FROM `set` ORDER BY `date` DESC") or die(mysql_error());
     $names = array();
     
     while($row = mysql_fetch_row($res)){
@@ -52,7 +77,17 @@ function DB_GetAllProjectSetNames(){
     return $names;
 }
 
-function DB_GetAllProjectStates(){
+/** \brief See if a given project set even exists.
+ * \param projectSetName The name of the project set to test for.
+ * \return This returns a boolean stating if the project exists.
+ */
+function DB_GetProjectSetExists($projectSetName){
+    $res = mysql_query("SELECT `name` FROM `set` WHERE `name` = '" . mysql_real_escape_string($projectSetName) . "' LIMIT 1") or die(mysql_error());
+    $row = mysql_fetch_row($res);
+    return (boolean)$row;
+}
+
+function DB_GetAllProjectSetStates(){
     
 }
 
@@ -63,12 +98,8 @@ function DB_GetAllProjectStates(){
  * url. If the query did not work, false is returned.
  */
 function DB_GetProjectEntries($projectSetName){
-    if(mysql_query("SELECT `id`, `name`, `url` WHERE `setname` = `" . mysql_real_escape_string($projectSetName) . "`")){
-        return mysql_fetch_array();
-    }
-    else{
-        return false;
-    }
+    $res = mysql_query("SELECT `id`, `name`, `url` WHERE `setname` = `" . mysql_real_escape_string($projectSetName) . "`");
+    return mysql_fetch_array($res);
 }
 
 function DB_GetProjectEntriesWithVotes($projectSetName){
@@ -77,6 +108,21 @@ function DB_GetProjectEntriesWithVotes($projectSetName){
 
 function DB_GetEntryData($connData, $entryID){
     
+}
+
+/// \}
+
+/** \defgroup utils Miscillaneous Utilities
+ * \brief These are miscellaneous utilities to reduce duplicate code.
+ * \{
+ */
+
+/** \brief This function returns the string "true" or "false" depending on the
+ * input.
+ * \return The literal string of the boolean value.
+ */
+function UT_BoolString($boolVal){
+    return $boolVal ? "true" : "false";
 }
 
 /// \}
