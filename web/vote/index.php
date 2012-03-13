07c354f10df7bb2a6ef3c8d18813795057196b03
Net_C <mark@techfocus.net>
2012-03-12 22:17:16 -0600

    Further moving vote/index.php to a php loop display.

--- web/vote/index.php
+++ web/vote/index.php
@@ -8,16 +8,21 @@ require_once('../config.php');
 require_once('../components.php');
 require_once('../functions.php');
 
-$project = array(
-                 array('id', 'name'),
-                 array('id2', 'name2'),
-                 array('id3', 'name3')
+$projects = array(
+                 array('id' => 0,
+                       'name' => 'Something',
+                       'url' => 'http://lolcats'),
+                 array('id' => 1,
+                       'name' => 'Something else',
+                       'url' => 'www'),
+                 array('id' => 2,
+                       'name' => 'Something fun',
+                       'url' => 'wat')
                 );
 
 $PROJECT_ID = 0;
 $PROJECT_NAME = 1;
 ?>
-
 <html>
     <head>
         <title>Project Voting</title>
@@ -26,58 +31,24 @@ $PROJECT_NAME = 1;
     <body>
         <div id="header"><h1>Voting</h1></div>
         <div id="votingForm">
-            <form name="postVote" action="" method="post"> <?php foreach($projects as $i => $project) {
+            <form name="postVote" action="" method="post"> <?php foreach($projects as $project) {
             print('
-                <table>
-                    <tr id="projectName">
-                        <td><h2>' . $project[$i] . '</h2></td>
-                    </tr>
-                    <div id="sliderBox">');
-                        foreach($CRITERIA_DEFAULT as $criteria) { print('
-                        <tr>
-                            <td>' . $criteria[$CRITERIA_NAME] . '</td>
-                            <td><input type="range" name="p' . $project[$i] . '.' . $criteria[$CRITERIA_NAME]
-                            . '" value="0" min="-10" max="10" /></td>
-                        </tr>');
-                        } print('
-                    </div>
-                </table>');
+                <div class="project">
+                    <h2><span id="projectName">' . $project['name'] . '</span><span id="projectUrl"><a href="' . $project['url'] . '">' . $project['url'] . '</a></span></h2>
+                    <table>
+                        <div id="sliderBox">');
+                            foreach($CRITERIA_DEFAULT as $criteria) { print('
+                            <tr>
+                                <td>' . $criteria[$CRITERIA_NAME] . '</td>
+                                <td><input type="range" name="p' . $project['id'] . '.' . $criteria[$CRITERIA_NAME]
+                                . '" value="0" min="-10" max="10" /></td>
+                            </tr>');
+                            } print('
+                        </div>
+                    </table>
+                </div>');
             }
             ?>
-                <table>
-                    <tr id="projectName">
-                        <td><h2>Project 2 Name<?php echo $NAME ?></h2></td>
-                    </tr>
-                    <div id="sliderBox"><?php
-                        foreach($CRITERIA_DEFAULT as $criteria) {
-                            print('
-                        <tr>
-                            <td>' . $criteria[$CRITERIA_NAME] . '</td>
-                            <td><input type="range" name="p' . $project[1] . '.' . $criteria[$CRITERIA_NAME]
-                            . '" value="0" min="-10" max="10" /></td>
-                        </tr>');
-                            }
-                        ?>
-                    
-                    </div>
-                </table>
-                <table>
-                    <tr id="projectName">
-                        <td><h2>Project 3 Name<?php echo $NAME ?></h2></td>
-                    </tr>
-                    <div id="sliderBox"><?php
-                        foreach($CRITERIA_DEFAULT as $criteria) {
-                            print('
-                        <tr>
-                            <td>' . $criteria[$CRITERIA_NAME] . '</td>
-                            <td><input type="range" name="p' . $project[2] . '.' . $criteria[$CRITERIA_NAME]
-                            . '" value="0" min="-10" max="10" /></td>
-                        </tr>');
-                            }
-                        ?>
-                    
-                    </div>
-                </table>
                 <table>
                     <tr>
                         <td><input type="submit" name="submit" /></td>
